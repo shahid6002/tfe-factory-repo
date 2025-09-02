67022e7c92f497a81ca8f51c08a40b1f5a67f4ac
Shahid Mohammed <shahid6002@gmail.com>
2025-09-03 01:12:17 +0530

    Add storage account module test for az-storage-module feature

---
+++ environments/dev/storage_test/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  cloud {
+    organization = "VolTFE"
+    workspaces {
+      name = "tf-factory-repo"
+    }
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "create_test_resources" {
+  type    = bool
+  default = true
+}
+
+module "storage_test" {
+  source = "git::https://github.com/my-org/terraform-azure-storage.git?ref=feature/az-storage-module"
+
+  resource_group_name  = "rg-storage-dev"
+  location             = "eastus"
+
+  # Generate a unique storage account name (3–24 chars, lowercase only)
+  storage_account_name = "teststorage${substr(md5(timestamp()), 0, 6)}"
+
+  tags = {
+    environment = "dev"
+  }
+
+  count = var.create_test_resources ? 1 : 0
+}
